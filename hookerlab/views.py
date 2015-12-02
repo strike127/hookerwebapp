313de7f286ec27cb1435e9112c37c157ec6ca635
jphan <jphan127@gmail.com>
2015-12-02 16:50:24 -0500

    add register

--- hookerlab/views.py
+++ hookerlab/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     context = {}
@@ -36,3 +37,17 @@ def invalid_login(request):
 def logout(request):
     auth.logout(request)
     return render(request, 'logout.html')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/register_success/')
+    args = {}
+    args.update(csrf(request))
+    args['form'] = UserCreationForm()
+    return render(request,'register.html',args)
+
+def register_success(request):
+    return render(request, 'register_success.html')
