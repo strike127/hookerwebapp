f7ab874ce4bda6491c107b17100475faea5db77c
jphan <jphan127@gmail.com>
2015-12-03 19:03:57 -0500

    add crispy forms,tables

--- hookerlab/settings.py
+++ hookerlab/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'django_tables2',
+    'person'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -65,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request',
             ],
         },
     },
@@ -101,3 +105,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
