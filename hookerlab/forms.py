0c9b00e87f5b85fd671f43f867c9bf540216b22b
jphan <jphan127@gmail.com>
2015-12-03 19:03:00 -0500

    add full name, remove help text

--- hookerlab/forms.py
+++ hookerlab/forms.py
@@ -5,10 +5,18 @@ from django.contrib.auth.forms import UserCreationForm
 class myRegistrationForm(UserCreationForm):
     
     email = forms.EmailField(required=True)
+    full_name = forms.CharField(required=True)
+
     
+    def __init__(self, *args, **kwargs):
+        super(myRegistrationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['username', 'email', 'full_name', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+            
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'full_name', 'password1', 'password2')
         
     def save(self, commit=True):
         user = super(myRegistrationForm, self).save(commit=False)
