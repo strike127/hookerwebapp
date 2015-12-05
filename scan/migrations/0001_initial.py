d5389aebddc89d6154b600304db136614ff0c905
jphan <jphan127@gmail.com>
2015-12-04 19:27:33 -0500

    update

--- scan/migrations/0001_initial.py
+++ scan/migrations/0001_initial.py
@@ -13,15 +13,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Scan',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, verbose_name='ID', serialize=False)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('subject', models.CharField(max_length=10)),
                 ('compound', models.CharField(max_length=3)),
+                ('current_status', models.CharField(max_length=20)),
                 ('name', models.CharField(max_length=30, verbose_name='full name')),
                 ('email', models.EmailField(max_length=120)),
-                ('date', models.DateField(auto_now_add=True)),
+                ('request_date', models.DateField(auto_now_add=True)),
                 ('updated_date', models.DateField(auto_now=True)),
-                ('time', models.TimeField(auto_now_add=True)),
+                ('request_time', models.TimeField(auto_now_add=True)),
                 ('updated_time', models.TimeField(auto_now=True)),
+                ('date_for_scan', models.CharField(max_length=30)),
+                ('time_for_scan', models.CharField(max_length=30)),
             ],
         ),
     ]
