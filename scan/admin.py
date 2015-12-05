8e71353a77eda3fdc7dd2d5273924e66b84dd064
jphan <jphan127@gmail.com>
2015-12-04 19:23:27 -0500

    update display list

--- scan/admin.py
+++ scan/admin.py
@@ -5,7 +5,7 @@ from .forms import ScanForm
 from .models import Scan
 
 class RequestScanAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "name", "subject", "compound", "date", "time", "updated_date", "updated_time", "currstat"]
+    list_display = ["__str__", "name", "subject", "compound", "date_for_scan", "time_for_scan", "request_date", "request_time", "updated_date", "updated_time", "current_status"]
     form = ScanForm
         
 admin.site.register(Scan, RequestScanAdmin)
