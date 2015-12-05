1b879d27dc4ba6ba4a892def9ea499c0bccd2006
jphan <jphan127@gmail.com>
2015-12-04 19:22:16 -0500

    add status,request date, and request time temp fields

--- scan/models.py
+++ scan/models.py
@@ -4,17 +4,19 @@ from django.db import models
 class Scan(models.Model):
     subject = models.CharField(max_length = 10)
     compound = models.CharField(max_length = 3)
-    currstat = models.CharField(max_length=20)
+    current_status = models.CharField(max_length=20)
+    
     #get from request.user
     name = models.CharField(verbose_name="full name", max_length=30)
     # name = request.user
     email = models.EmailField(max_length=120)
     # email = name.email
-    date = models.DateField(auto_now_add=True, auto_now=False)
+    request_date = models.DateField(auto_now_add=True, auto_now=False)
     updated_date = models.DateField(auto_now_add=False, auto_now=True)
-    time = models.TimeField(auto_now_add=True, auto_now=False)
+    request_time = models.TimeField(auto_now_add=True, auto_now=False)
     updated_time = models.TimeField(auto_now_add=False, auto_now=True)
-    
+    date_for_scan = models.CharField(max_length = 30)
+    time_for_scan = models.CharField(max_length = 30)
 
     def __str__(self):
         return self.email
