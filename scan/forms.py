290af3aedbdc03559476fad9bc07cfa3d6112326
jphan <jphan127@gmail.com>
2015-12-07 17:42:11 -0500

    add help text

--- scan/forms.py
+++ scan/forms.py
@@ -7,7 +7,11 @@ class ScanForm(forms.ModelForm):
         # fields = ['email', 'name', 'subject', 'compound', 'current_status', 'date_for_scan', 'time_for_scan']
         # fields = ['subject', 'compound', 'date_for_scan', 'time_for_scan']
         exclude = ['email', 'name', 'current_status']
-    
+        
+        help_texts = {
+            'subject': 'Group to which this message belongs to',
+            'compound': 'C11/F18',
+        }
     # def save(self, commit=True):
     #     scan = super(ScanForm, self).save(commit=False)
     #     scan.email = self.cleaned_data['email']
