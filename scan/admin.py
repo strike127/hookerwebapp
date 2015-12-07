b2ea3e22359d1d88fd1f2c5052855234423eb65b
jphan <jphan127@gmail.com>
2015-12-07 15:36:57 -0500

    renamed a column ___str___ to email

--- scan/admin.py
+++ scan/admin.py
@@ -5,7 +5,7 @@ from .forms import ScanForm
 from .models import Scan
 
 class RequestScanAdmin(admin.ModelAdmin):
-    list_display = [ "name", "__str__", "subject", "compound", "date_for_scan", "time_for_scan", "request_date", "request_time", "updated_date", "updated_time", "current_status"]
+    list_display = [ "name", "email", "subject", "compound", "date_for_scan", "time_for_scan", "request_date", "request_time", "updated_date", "updated_time", "current_status"]
     form = ScanForm
         
 admin.site.register(Scan, RequestScanAdmin)
