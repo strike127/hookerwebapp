e9ab5368dd7c9b0469160754b8200840ee38ac5d
jphan <jphan127@gmail.com>
2015-12-07 15:14:05 -0500

    remove email,name,and status from form

--- scan/forms.py
+++ scan/forms.py
@@ -4,13 +4,14 @@ from .models import Scan
 class ScanForm(forms.ModelForm):
     class Meta:
         model = Scan
-        fields = ['email', 'name', 'subject', 'compound', 'current_status', 'date_for_scan', 'time_for_scan']
-        
-    def save(self, commit=True):
-        scan = super(ScanForm, self).save(commit=False)
-        scan.email = self.cleaned_data['email']
-        
-        if commit:
-            scan.save()
-        
-        return scan
+        # fields = ['email', 'name', 'subject', 'compound', 'current_status', 'date_for_scan', 'time_for_scan']
+        # fields = ['subject', 'compound', 'date_for_scan', 'time_for_scan']
+        exclude = ['email', 'name', 'current_status']
+    # def save(self, commit=True):
+    #     scan = super(ScanForm, self).save(commit=False)
+    #     scan.email = self.cleaned_data['email']
+    #     
+    #     if commit:
+    #         scan.save()
+    #     
+    #     return scan
