ef8ac6451dfb64d70d65b77ceabc95549acbc0a6
jphan <jphan127@gmail.com>
2015-12-09 17:53:21 -0500

    include synthesis columns

--- scan/admin.py
+++ scan/admin.py
@@ -5,7 +5,9 @@ from .forms import ScanForm
 from .models import Scan
 
 class RequestScanAdmin(admin.ModelAdmin):
-    list_display = [ "name", "email", "subject", "compound", "date_for_scan", "time_for_scan", "request_date", "request_time", "updated_date", "updated_time", "current_status"]
+    list_display = [ "name", "email", "subject", "compound", "date_for_scan",
+                    "time_for_scan", "request_date", "request_time", "updated_date", "updated_time",
+                    "init_activity", "synthesize_time", "synthesize_time_auto", "toi", "scan_time", "scan_time_auto", "current_status"]
     form = ScanForm
         
 admin.site.register(Scan, RequestScanAdmin)
