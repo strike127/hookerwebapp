d1e58396cf8b97709c076a50ca03f6e008462031
jphan <jphan127@gmail.com>
2015-12-14 18:44:24 -0500

    update

--- scan/admin.py
+++ scan/admin.py
@@ -7,7 +7,8 @@ from .models import Scan
 class RequestScanAdmin(admin.ModelAdmin):
     list_display = [ "name", "email", "subject", "compound", "date_for_scan",
                     "time_for_scan", "request_date", "request_time", "updated_date", "updated_time",
-                    "init_activity", "synthesize_time", "synthesize_time_auto", "toi_activity", "scan_time", "scan_time_auto", "current_status"]
+                    "init_activity", "synthesize_time", "synthesize_time_auto", "toi_activity",
+                    "scan_time", "scan_time_auto", "current_status"]
     form = ScanForm
         
 admin.site.register(Scan, RequestScanAdmin)
